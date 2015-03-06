f158e8d48f47f1f5c9cbb0101ca8d12915642cc2
Reynaldo <kingmm2011@gmail.com>
2015-03-05 18:10:43 -0600

    Creada la pagina de inicio

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Prueba Git</title>
+	</head>
+
+    <body>
+    	<!--INICIO DE CODIGO HTML5-->
+        <h1>Bienvenidos</h1>
+    </body>
+</html>
